36b8410c1014352dc7be18f0189fb46260569852
Ali Mustadji <101158664+L1mus@users.noreply.github.com>
2026-02-27 21:33:42 +0700

    Try to fix bug

--- Minitask-11.js
+++ Minitask-11.js
@@ -5,46 +5,41 @@ batasan dan kendala : setelah 1500,2000,500
 */
 
 
-const data = [
-  {name:"John"},
-  {name:"Ed"},
-  {name:"Jane"},
-];
+const data = ["john","ed","jane"];
 
 const time = [500,1500,2000];
 
-const showName = (data,time) => {
-  return new Promise((resolve,reject)=>{
-    const dataArr =
-     Object.values(data)
-    setTimeout(()=>{
-      for(let i of dataArr){
-        resolve([i])
-      }      
-    },()=>{
-      for(let i of time){
-        return [i]
-      }})
-   reject("Fetch data gagal")
-  })
-};
+data.forEach((value)=>{
+  const janjiJiwa = (time)=>{
 
+    return new Promise((resolve,reject)=>{
+        setTimeout(()=>{
+              return resolve(value)
+    },time)
+  }
 
-showName(data,...time)
-  .then((data,time)=>{
-    console.log(data)
+  janjiJiwa(500)
+  .then((value)=>{
+    console.log(value)
   })
-  // .then()
-  .catch(err =>{
+  .catch(err=>{
     console.log(err)
   })
+})
 
-const asyncAwaitFn = async () =>{
-  try{
-    const result = await showName()
-    console.log(`${result}`)
+
+
+data.forEach((value)=>{
+  const janjiJiwa = (time)=>{
+    return new Promise((resolve,reject)=>{
+        setTimeout(()=>{
+              return resolve(value)
+    },time)
+    return reject("Fetch gagal")})
   }
-  catch(err){
-    console.log(`${err}`)
-  }
+
+const asyncAwait = async () => {
+  const data = await jajiJiwa(500)
+  console.log(data)
 }
+})
