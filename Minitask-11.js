14b4d1752d1e2369f0feb51ef8df5e4e0cbf7c77
Ali Mustadji <101158664+L1mus@users.noreply.github.com>
2026-02-27 15:35:06 +0700

    Create Minitask-11.js

---
+++ Minitask-11.js
@@ -0,0 +1,40 @@
+/*
+input: nama : string
+output : logging text string name
+batasan dan kendala : setelah 1500,2000,500
+*/
+
+
+const data = [
+  {nama:"John"},
+  {nama:"Ed"},
+  {nama:"Jane"},
+];
+
+console.log(data[0].nama)
+
+const showName = (data) =>{
+  return new Promises((resolve,reject)=>{
+    setTimeout(()=>{
+        resolve(data[0].name)
+    },1500)
+   reject("Fetch data gagal")
+  })
+};
+
+
+// showName()
+//   .then()
+//   .catch()
+
+const asyncAwaitFn = async () =>{
+  try{
+    const result = await showName()
+    console.log(`${result}`)
+  }
+  catch(err){
+    console.log(`${err}`)
+  }
+   const result = await showName()
+   return result
+}
